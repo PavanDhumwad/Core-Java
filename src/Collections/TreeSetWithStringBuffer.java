d16c74c8adcbb71e1cc040bf10356de2debaecc2
PavanDhumwad <pgd.1996@gmail.com>
2019-12-22 16:49:54 +0530

    TreeSet with class not providing implementation of Comparable intref.

---
+++ src/Collections/TreeSetWithStringBuffer.java
@@ -0,0 +1,27 @@
+package Collections;
+
+import java.util.Comparator;
+import java.util.TreeSet;
+
+public class TreeSetWithStringBuffer
+{
+    public static void main(String[] args) {
+        TreeSet t = new TreeSet(new SBNotImpComparableIntrf());
+        System.out.println("StringBuffer does not implement Comparable interf. So we need provide " +
+                "implementation of Comparator interf");
+        t.add(new StringBuffer("A"));
+        t.add(new StringBuffer("C"));
+        t.add(new StringBuffer("B"));
+        System.out.println(t);
+    }
+}
+class SBNotImpComparableIntrf implements Comparator
+{
+
+    @Override
+    public int compare(Object o1, Object o2) {
+        String s1 = o1.toString();
+        String s2 = o2.toString();
+        return s1.compareTo(s2);
+    }
+}
