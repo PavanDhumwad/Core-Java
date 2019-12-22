6c5d53c94ced2a2aacf2aebd934c3c9f59712e2f
PavanDhumwad <pgd.1996@gmail.com>
2019-12-23 00:15:28 +0530

    TreeSet with class not providing implementation of Comparable intref.

--- src/Collections/TreeSetWithStringBuffer.java
+++ src/Collections/TreeSetWithStringBuffer.java
@@ -5,6 +5,7 @@ import java.util.TreeSet;
 
 public class TreeSetWithStringBuffer
 {
+
     public static void main(String[] args)
     {
         TreeSet t = new TreeSet(new SBNotImpComparableIntrf());
